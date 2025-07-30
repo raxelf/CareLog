0377a532087ee4f0406707e705af330247d3d3aa
raxelf <frananda.rafael@gmail.com>
2025-07-31 02:13:05 +0700

    fix: adjusted some routing, styling

--- routers/router.js
+++ routers/router.js
@@ -3,7 +3,13 @@ const Controller = require('../controllers/controller');
 const router = express.Router();
 
 router.get('/', Controller.getHome);
+
+// Auth
 router.get('/login', Controller.getLogin);
+router.post('/login', Controller.X);
 router.get('/register', Controller.getRegister);
+router.post('/register', Controller.X);
+
+
 
 module.exports = router;
